582b115acc080bd237a4364774a98c5302252916
blaycoder <onatola4real@gmail.com>
2025-01-01 21:30:10 +0100

    removed unneccessary comments

--- day-1/CounterII/script.js
+++ day-1/CounterII/script.js
@@ -14,14 +14,3 @@ let createCounter = function (init) {
   };
 };
 
-const counter = createCounter(5);
-console.log(counter.increment()); // 6
-console.log(counter.reset()); // 5
-console.log(counter.decrement()); // 4
-
-const counterOne = createCounter(0);
-console.log(counterOne.increment());
-console.log(counterOne.increment());
-console.log(counterOne.decrement());
-console.log(counterOne.reset());
-console.log(counterOne.reset());
