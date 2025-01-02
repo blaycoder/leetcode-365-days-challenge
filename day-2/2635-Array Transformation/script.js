9e6eecb144bf843009d43a4d0f2bbc4224a97a8c
blaycoder <onatola4real@gmail.com>
2025-01-02 20:42:49 +0100

    made changes to script

--- day-2/2635-Array Transformation/script.js
+++ day-2/2635-Array Transformation/script.js
@@ -1,11 +1,13 @@
+// Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+// // The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+
 let map = function (arr, fn) {
   const newArr = [];
   for (let i = 0; i < arr.length; i++) {
-    if (fn.length === 1) {
-      newArr.push(fn(arr[i]));
-    }
-    else if (fn.length === 0) {
+    if (fn.length === 0) {
       newArr.push(fn());
+    } else if (fn.length === 1) {
+      newArr.push(fn(arr[i]));
     } else {
       newArr.push(fn(arr[i], i));
     }
@@ -30,7 +32,3 @@ console.log(map(arrNum, plusone));
 
 console.log(map(arrNum, plusI));
 console.log(map(arrNum, constant));
-
-// Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
-// // The returned array should be created such that returnedArray[i] = fn(arr[i], i).
-// Please solve it without the built-in Array.map method.
