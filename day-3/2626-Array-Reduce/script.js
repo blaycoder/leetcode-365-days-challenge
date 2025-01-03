84c6bd1242cf38a4666c4ae334b8d9e997d23320
blaycoder <onatola4real@gmail.com>
2025-01-03 22:47:01 +0100

    updated script

--- day-3/2626-Array-Reduce/script.js
+++ day-3/2626-Array-Reduce/script.js
@@ -1,10 +1,15 @@
+// Given an integer array nums, a reducer function fn, and an initial value init, return the final result obtained by executing the fn function on each element of the array, sequentially, passing in the return value from the calculation on the preceding element.
+
+// This result is achieved through the following operations: val = fn(init, nums[0]), val = fn(val, nums[1]), val = fn(val, nums[2]), ... until every element in the array has been processed. The ultimate value of val is then returned.
+
+// If the length of the array is 0, the function should return init.
+
+// Please solve it without using the built-in Array.reduce method.
+
 let reduce = function (nums, fn, init) {
   let acc = init;
   if (nums.length >= 1) {
     for (let i = 0; i < nums.length; i++) {
-      if (fn.length === 0) acc = fn();
-      else if (fn.length === 1) acc = fn(nums[i]);
-      else if (fn.length === 2) acc = fn(acc, init, nums[i]);
       acc = fn(acc, nums[i]);
     }
     return acc;
@@ -13,9 +18,9 @@ let reduce = function (nums, fn, init) {
   }
 };
 
-nums = [1, 2, 3, 4];
-fn = function sum(accum, curr) {
-  return accum + curr;
-};
-init = 0;
-console.log(reduce(nums, fn, init));
+// nums = [1, 2, 3, 4];
+// fn = function sum(accum, curr) {
+//   return accum + curr * curr;
+// };
+// init = 100;
+// console.log(reduce(nums, fn, init));
