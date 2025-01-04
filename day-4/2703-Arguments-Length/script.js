4808044615fc0e6d5704452a2cbecb116dbedcab
blaycoder <onatola4real@gmail.com>
2025-01-04 22:26:19 +0100

    added day-4 script

---
+++ day-4/2703-Arguments-Length/script.js
@@ -0,0 +1,7 @@
+// Write a function argumentsLength that returns the count of arguments passed to it.
+
+let argumentsLength = function (...args){
+    return args.length;
+}
+
+console.log(argumentsLength({}, null, "3")); 
