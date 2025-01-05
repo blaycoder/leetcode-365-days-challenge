be2eb0c02bcbe3666ba676925c8197b7a033cba1
blaycoder <onatola4real@gmail.com>
2025-01-05 20:40:15 +0100

    moved script

--- day-4/2703-Arguments-Length/script.js
+++ day-4/2703-Arguments-Length/script.js
@@ -1,7 +1,7 @@
 // Write a function argumentsLength that returns the count of arguments passed to it.
 
-let argumentsLength = function (...args){
-    return args.length;
-}
+let argumentsLength = function (...args) {
+  return args.length;
+};
 
-console.log(argumentsLength({}, null, "3")); 
+console.log(argumentsLength({}, null, "3"));
