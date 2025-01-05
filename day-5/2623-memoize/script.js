f9230b15dc7f3ad7826bed4a627efbe9f296832b
blaycoder <onatola4real@gmail.com>
2025-01-05 23:09:45 +0100

    updated script

--- day-5/2623-memoize/script.js
+++ day-5/2623-memoize/script.js
@@ -1,31 +1,44 @@
+// Given a function fn, return a memoized version of that function.
+
+// A memoized function is a function that will never be called twice with the same inputs. Instead it will return a cached value.
+
+// You can assume there are 3 possible input functions: sum, fib, and factorial.
+
+// sum accepts two integers a and b and returns a + b. Assume that if a value has already been cached for the arguments (b, a) where a != b, it cannot be used for the arguments (a, b). For example, if the arguments are (3, 2) and (2, 3), two separate calls should be made.
+// fib accepts a single integer n and returns 1 if n <= 1 or fib(n - 1) + fib(n - 2) otherwise.
+// factorial accepts a single integer n and returns 1 if n <= 1 or factorial(n - 1) * n otherwise.
+
 function memoize(fn) {
-  let called = false;
-  let prevArgs = [];
-  let cachedResult;
+  let cache = new Map();
+  let callCount = 0;
 
   return function (...args) {
-    if (
-      called &&
-      prevArgs.length === args.length &&
-      prevArgs.every((value, index) => value === args[index])
-    ) {
-      console.log("Result is cached");
-      return cachedResult;
-    } else {
-      called = true;
-      prevArgs = [...args];
-      cachedResult = fn(...args);
-      return cachedResult;
+    if (args.length === 0) {
+      return callCount;
     }
+
+    const key = JSON.stringify(args);
+
+    if (cache.has(key)) {
+      console.log("Result is cached");
+      return cache.get(key);
+    }
+    callCount++;
+    const result = fn(...args);
+    cache.set(key, result);
+
+    return result;
   };
 }
 
-const sum = (a, b) => a + b;
-const memoizedSum = memoize(sum);
-console.log(memoizedSum(2, 2));
-console.log(memoizedSum(2, 2));
-console.log(memoizedSum(1, 2));
+// Test Cases
 
-// const factorial = (n) => (n <= 1 ? 1 : n * factorial(n - 1));
-// const memoFactorial = memoize(factorial);
-// console.log(memoFactorial(3));
+const factorial = (n) => (n <= 1 ? 1 : n * factorial(n - 1));
+const memoFactorial = memoize(factorial);
+
+console.log(memoFactorial(2));
+console.log(memoFactorial(3));
+console.log(memoFactorial(2));
+console.log(memoFactorial());
+console.log(memoFactorial(3));
+console.log(memoFactorial());
